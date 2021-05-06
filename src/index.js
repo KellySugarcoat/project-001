0033e88f3aa5185e6efa6847ef7d15b50850b9db
Catalina uwu <kellysugarcoat@gmail.com>
2021-05-06 13:50:16 -0300

    fix server

--- src/index.js
+++ src/index.js
@@ -1,10 +1,8 @@
 const express = require('express');
 const app = express();
-const portser = 80;
-const path = require('path');
+const portser = 3333;
 
 //Server
-app.set('view engine', 'ejs');
 
 
 app.listen(portser, () => {
@@ -12,5 +10,5 @@ app.listen(portser, () => {
 })
 
 app.get('/', (req, res) =>{
-res.render('index', path.join(__dirname + '/../index.html'));
+    res.send('hi uwu')
 })
