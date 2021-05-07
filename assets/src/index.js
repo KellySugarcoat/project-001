4a587a130380bb83dc3721653648bf365f95a61c
FlaconVerde <Laupa88@hotmail.com.ar>
2021-05-07 19:47:33 -0300

    NordicoLatino

--- assets/src/index.js
+++ assets/src/index.js
@@ -62,7 +62,7 @@ const testGalerry = [
         pic: 'https://i.pinimg.com/originals/2c/36/d1/2c36d1ab84a3d178a7de6e7789f5b727.jpg'
     },
     {
-        desc: 'TEEMO ES CUTE',
+        des: 'TEEMO ES CUTE',
         pic: 'https://esports.as.com/2018/05/01/league-of-legends/Beemo-Pulso-Fuego-League-Legends_1131796817_70275_1024x576.jpg'
     },
     {
