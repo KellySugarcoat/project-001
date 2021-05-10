efec86497c745606b33909de2ca84932ffd5de11
FlaconVerde <Laupa88@hotmail.com.ar>
2021-05-10 19:32:26 -0300

    NordicoLatino

--- assets/src/index.js
+++ assets/src/index.js
@@ -56,20 +56,7 @@ const userFromDatabase = {
     hobbies: ['Pasar tiempo con el uwu', 'LOL', 'stress']
 }
 
-const testGalerry = [
-    {
-        desc: 'IRELIA ES REINA',
-        pic: 'https://i.pinimg.com/originals/2c/36/d1/2c36d1ab84a3d178a7de6e7789f5b727.jpg'
-    },
-    {
-        des: 'TEEMO ES CUTE',
-        pic: 'https://esports.as.com/2018/05/01/league-of-legends/Beemo-Pulso-Fuego-League-Legends_1131796817_70275_1024x576.jpg'
-    },
-    {
-        desc: 'mh ah mh mh uh ah',
-        pic: 'https://ddragon.leagueoflegends.com/cdn/img/champion/splash/Malphite_0.jpg'
-    },
-]
+const testGalery = require('./assets/js/galery')
 
 app.get('/test', (req, res) => res.render('TestView', { user: userFromDatabase }))
 
